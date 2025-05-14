e2e3021c936db697d05cf985f5abf856ff359f3c
Sazid <sazidtec06@gmail.com>
2025-05-15 00:56:36 +0600

    book.css added

--- src/Book.jsx
+++ src/Book.jsx
@@ -1,7 +1,8 @@
+import './Book.css'
 export default function Book ({book}){
     const {name, score} = book;
     return (
-        <div>
+        <div className='book'>
             <h3>Book Name : {name}</h3>
             <h3>Score : {score}</h3>
         </div>
